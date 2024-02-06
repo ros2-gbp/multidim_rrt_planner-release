4e33728719cbb38c94b7658a74ddfd84f7e58f33
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2024-02-05 20:20:41 -0500

    Rebase from 'release/iron/multidim_rrt_planner'

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = 'multidim_rrt_planner'
 
 setup(
     name=package_name,
-    version='0.0.6',
+    version='0.0.7',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
